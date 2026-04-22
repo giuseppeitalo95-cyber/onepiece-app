81abc95803f79e2bb487446b231f3ed76ce677ed
Giuseppe <giuseppeitalo95@gmail.com>
2026-04-22 20:59:34 +0200

    update login redirect

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo: 'http://localhost:3000/auth/callback'
+redirectTo: 'https://onepiece-app-one.vercel.app/auth/callback'
     }
   })
 }
